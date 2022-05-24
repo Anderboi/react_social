f0361572fb7e2b11318fe9a61bdecc23bebf0811
Anderboi <upolovnew@gmail.com>
2022-05-24 19:41:04 +0300

    clean App for nex work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
 
-const App = () => {
+
+function App() {
   return (
     <div className="App">
-      JUST DO IT
-      <ul className="nav">
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-      </ul>
+      
     </div>
   );
-};
+}
 
 export default App;
